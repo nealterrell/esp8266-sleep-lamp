1641b4954b1a04897d587e00e9c07e2f93476240
Neal Terrell <neal.terrell@csulb.edu>
2020-07-17 20:28:54 -0700

    Move constants into config file.

--- LampConfig.h
+++ LampConfig.h
@@ -11,9 +11,19 @@ constexpr uint8_t NAP_WAKE_DURATION = 15;
 // True if the light should automatically set mode to SLEEPING and a wake time of to NIGHT_WAKE_XX
 //    at the trigger time below.
 constexpr bool AUTO_SLEEP = true;
-// The hour and minute at which the light automatically goes to "sleep", if AUTO_SLEEP is true.
+// The hour and minute at which the light automatically goes to "sleep", if AUTO_SLEEP is true and mode is OFF.
 constexpr uint8_t AUTO_SLEEP_HOUR = 20;
 constexpr uint8_t AUTO_SLEEP_MINUTE = 0;
+
+constexpr uint32_t RAINBOW_STEPS = 100;
+constexpr uint32_t RAINBOW_DURATION = 5000;
+
+/******************* Pins and constants *******************/ 
+constexpr size_t NUM_LIGHTS = 2;
+constexpr uint8_t RED_PINS[NUM_LIGHTS]   = {13, 2};
+constexpr uint8_t GREEN_PINS[NUM_LIGHTS] = {12, 0};
+constexpr uint8_t BLUE_PINS[NUM_LIGHTS]  = {14, 4};
+
 
 
 // Remove the next line if you are using common cathode LEDs.
